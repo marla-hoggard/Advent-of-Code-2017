91485dc9f1654133bbb8acf7f0adea76cee37c55
Marla Glasser <32146632+mglasser@users.noreply.github.com>
2018-02-20 18:16:31 -0500

    Update and rename Day-2-Puzzle-2.js to src/Day-2-Puzzle-2.js

--- src/Day-2-Puzzle-2.js
+++ src/Day-2-Puzzle-2.js
@@ -1,13 +1,16 @@
 //Takes an array of arrays
 //For each array, find the pair where one is a multiple of the other
-//Return the sum of a/b from each row where a & b are the pair, a > b
+//Return the sum of b/a from each row where a & b are the pair, b > a
 function checksumDiv(matrix) {
 	var array = [];
 	var sum = 0;
+	//Iterate through each row
 	for (var i = 0, n = matrix.length; i < n; i++) {
 		array = matrix[i];
 		var len = array.length;
+		//Sort ascending so we know b > a
 		array.sort(function(a,b) {return a-b;});
+		//We know there's exactly one pair, so break for loop once it's been found
 		var found = false;
 		for (var a = 0; a < len - 1 && !found; a++) {
 			for (var b = a+1; b < len; b++) {
