a380a11ef1402edf9051490c6da73bba9ae7be38
Marla Glasser <32146632+mglasser@users.noreply.github.com>
2018-02-20 18:02:59 -0500

    Update Day-1-Puzzle-2.js

--- src/Day-1-Puzzle-2.js
+++ src/Day-1-Puzzle-2.js
@@ -4,11 +4,13 @@ function captcha_round(n) {
 	var array = ("" + n).split("");
 	var sum = 0;
 	var len = array.length;
+	//Iterate through first half of input, checking matching digit len/2 away
 	for (var i = 0; i < len/2; i++) {
 		if (array[i] === array[i + (len/2)]) {
 			sum += +array[i];
 		}
 	}
 	
+	//Double the found sum to account for second half of input (which must match first half sum)
 	return sum*2;
 }
