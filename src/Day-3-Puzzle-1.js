5d7ae8c234ee9191cd79962c181bb25dde5d6c15
Marla Glasser <32146632+mglasser@users.noreply.github.com>
2018-02-20 18:26:24 -0500

    Update Day-3-Puzzle-1.js

--- src/Day-3-Puzzle-1.js
+++ src/Day-3-Puzzle-1.js
@@ -7,16 +7,22 @@ function spiralDist(n) {
 	if (n === 1) {
 		return 0;
 	}
-	var ring = 2;
-	var i = 3;
+	var ring = 2; //the first ring is just 1 (the center)
+	var i = 3; //ring two has side-length 3
 	
 	//Figure out what ring n is in
+	//The highest n in a given ring is i^2 where i = side-length of that ring
 	while(n > i*i) {
 		ring++;	
-		i+=2;
+		i+=2; //side-length goes up by two as rings increase
 	}
 	
 	var diff = (i*i) - n; //distance from i*i in the ring
-	var size = i-1;
+	var size = i-1; //Each side has length i but that double-counts corners
+	
+	//ring-1 = distance outwards from center
+	//size/2 = distance from corner to center of ring's side
+	//diff % size = how far from the corner is n
+	//Math.abs((size/2) - (diff % size))) = (full distance from corner to center) - (how far from corner is n)
 	return ((ring-1) + Math.abs((size/2) - (diff % size)));
 }
