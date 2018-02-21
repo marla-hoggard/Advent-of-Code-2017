f2c695a69114f8458912fc0e8e3e96f3bc794ef2
Marla Glasser <32146632+mglasser@users.noreply.github.com>
2018-02-20 19:57:36 -0500

    Create Day4-Puzzle1.js

---
+++ src/Day4-Puzzle1.js
@@ -0,0 +1,27 @@
+//Advent of Code - Day 4 - Puzzle 1
+//Takes an array of strings, each representing a pw input
+//Returns number of pw strings with no duplicate words
+function checkPW(input) {
+	var valid = 0;
+	for (var i = 0, n = input.length; i < n; i++) {
+		if (checkDup(input[i])) {
+			valid++;
+		}
+	}
+	return valid;
+}
+
+//Advent of Code - Day 4 - Puzzle 1
+//Takes string of words separated by spaces
+//Returns true if no duplicate words in sentence
+function checkDup(string) {
+	var words = string.split(" ");
+	var checked = [];
+	for (var i = 0, n = words.length; i < n; i++) {
+		if (!checked.includes(words[i])) {
+			checked.push(words[i]);
+		}
+		else return false;
+	}
+	return true;
+}
