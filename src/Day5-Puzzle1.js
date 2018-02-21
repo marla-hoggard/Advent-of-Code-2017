8eedf70b37f8e9eff9e0a32ae311485a42da9c3f
Marla Glasser <32146632+mglasser@users.noreply.github.com>
2018-02-20 20:27:47 -0500

    Update Day5-Puzzle1.js

--- src/Day5-Puzzle1.js
+++ src/Day5-Puzzle1.js
@@ -4,9 +4,10 @@ function escapeMaze(instr) {
 	var i = 0;
 	var n = instr.length;
 	var steps = 0;
+	//Keep going until escape maze aka i >= n
 	while (i < n) {
 		instr[i]++;
-		i += instr[i] - 1;
+		i += instr[i] - 1; //-1 because instr[i] has already been incremented
 		steps++;
 	}
 	return steps;
