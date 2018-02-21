3569a4688b141d9203dda9bdcfcc5f28e167b128
Marla Glasser <32146632+mglasser@users.noreply.github.com>
2018-02-20 20:28:47 -0500

    Update Day5-Puzzle2.js

--- src/Day5-Puzzle2.js
+++ src/Day5-Puzzle2.js
@@ -4,15 +4,16 @@ function escapeMaze2(instr) {
 	var i = 0;
 	var n = instr.length;
 	var steps = 0;
+	//Maze is escaped when i >= n (since n is instr.length aka the maze's length)
 	while (i < n) {
 		if (instr[i] >= 3) {
 			instr[i]--;
-			i += instr[i] + 1;
+			i += instr[i] + 1; //+1 since instr[i]--
 			steps++;
 		}
 		else {
 			instr[i]++;
-			i += instr[i] - 1;
+			i += instr[i] - 1; //-1 since instr[i]++
 			steps++;
 		}
 	}
