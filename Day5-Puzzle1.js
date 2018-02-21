8440ed4023e9320d2a502f150afc12c172bab21d
Marla Glasser <32146632+mglasser@users.noreply.github.com>
2018-02-20 20:17:19 -0500

    Create Day5-Puzzle1.js

---
+++ Day5-Puzzle1.js
@@ -0,0 +1,13 @@
+//Takes array of instructions
+//Returns number of steps to get out of maze
+function escapeMaze(instr) {
+	var i = 0;
+	var n = instr.length;
+	var steps = 0;
+	while (i < n) {
+		instr[i]++;
+		i += instr[i] - 1;
+		steps++;
+	}
+	return steps;
+}
