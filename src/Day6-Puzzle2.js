a72816a25aff3a476538ef0f2abf191954a37035
Marla Glasser <32146632+mglasser@users.noreply.github.com>
2018-02-20 20:51:49 -0500

    Update Day6-Puzzle2.js

--- src/Day6-Puzzle2.js
+++ src/Day6-Puzzle2.js
@@ -1,6 +1,7 @@
 //Takes string of numbers separated by whitespace
-//Runs redistribution pattern
-//Returns number of iterations before finding a repeat
+//Runs redistribution pattern until a repeat is found
+//Returns size of loop (iterations between repeated distribution)
+//NOTE: Identical to Puzzle 1 except for the return statement
 function redistributeDiff(input) {
 	//Make thisRound an array of integers
 	var thisRound = input.split(/\s/);
@@ -9,18 +10,19 @@ function redistributeDiff(input) {
 		thisRound[i] *=1;
 	}
 	var rounds = [];
-	var thisRoundString = thisRound.join(" ");
+	var thisRoundString = thisRound.join(" "); //Easier to search for duplicates to find repeat
 	var times = 0;
 	do {
 		rounds.push(thisRoundString);
 		var maxFacts = arrayMax(thisRound);
 		var amountDist, amountLeft;
+		
 		//At least one is distributed everywhere else
 		if (maxFacts[0] >= (banks-1)) {
-			amountDist = Math.floor(maxFacts[0] / (banks-1));
-			amountLeft = maxFacts[0] - (amountDist * (banks-1));
+			amountDist = Math.floor(maxFacts[0] / (banks-1)); //how much goes to each other bank
+			amountLeft = maxFacts[0] - (amountDist * (banks-1)); //how much the original keeps (remainder)
 			for (var r = 0; r < banks; r++) {
-				if (r == maxFacts[1]) {
+				if (r == maxFacts[1]) { //r is index of distributing bank
 					thisRound[r] = amountLeft;
 				}
 				else {
@@ -32,19 +34,34 @@ function redistributeDiff(input) {
 		else {
 			amountDist = maxFacts[0];
 			var index = maxFacts[1];
-			thisRound[index] = 0;
+			thisRound[index] = 0; //No remainder, distributing bank goes to 0
 			index++;
 			for (index; amountDist > 0; amountDist--) {
 				if (index == banks) { //Wrap around
 					index = 0;
 				}
-				thisRound[index]++;
+				thisRound[index]++; //Add one to the selected bank
 				index++;
 			}
 		}
 		thisRoundString = thisRound.join(" ");
 		times++;
 	}
-	while (!rounds.includes(thisRoundString));
-	return (times - rounds.indexOf(thisRoundString));
+	while (!rounds.includes(thisRoundString)); //Keep going until thisRoundString matches one we've already seen aka repeat/inf loop
+	return (times - rounds.indexOf(thisRoundString)); //Space between this round (times) and the first appearance of the pattern
 }
+
+//Takes an array of nummbers (or number strings)
+//returns [max, index]
+//Where max = max value, index = first index of max
+function arrayMax(array) {
+	var max = +array[0];
+	var index = 0;
+	for (var i = 0, n = array.length; i < n; i++) {
+		if (+array[i] > max) {
+			max = +array[i];
+			index = i;
+		}
+	}
+	return [max, index];
+ }
