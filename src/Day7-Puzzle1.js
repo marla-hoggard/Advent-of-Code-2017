2c3ba8d0a817ec894de530ec554ea678856cb289
Marla Glasser <32146632+mglasser@users.noreply.github.com>
2018-02-21 23:30:03 -0500

    Update Day7-Puzzle1.js

--- src/Day7-Puzzle1.js
+++ src/Day7-Puzzle1.js
@@ -1,36 +1,30 @@
-//Takes an array, with inputs of each tower as a string
+//Takes the puzzle input as an array of tower strings, i.e. "emlzcpy (106) -> pwmoihf, sdwnkb"
 //Returns the name of the bottom tower
 function bottomTower(towers) {
-	//Filter to only towers that carry something
-	var carriers = towers.filter(function(elem) {
-		return elem.includes('->');
-	});
+
+	//Create an array of the towers that carry something
+	var carriers = towers.filter(elem =>  elem.includes('->'));
+	
 	//trim whitespace from each tower
-	carriers.forEach(function(elem, index) {
-		carriers[index] = elem.trim();
-	});
+	carriers = carriers.map(elem => elem.trim());
 	
 	//Create an array carrierObjs that contains each carrier as an object with prop name, weight and carrying
-	var carrierObjs = [];
-	for (var i = 0, n = carriers.length; i < n; i++) {
-		var carrierArray = carriers[i].split(" ");
-		var carrying = (carrierArray.slice(3)).join("");
-		carrying = carrying.split(",");
-		
-		var towerObj = new tower(carrierArray[0],
-														 carrierArray[1],carrying);
-		carrierObjs.push(towerObj);
-	}
-	console.log(carrierObjs);
+	var carrierObjs = carriers.map(carrier => {
+		var carrierArray = carrier.split(" "); //split this carrier by whitespace
+		var carrying = (carrierArray.slice(3)).join("").split(","); //an array of what it's carrying
+		return new Tower(carrierArray[0], carrierArray[1],carrying);		
+	});
+	
 	//Make carried an array of the towers that are carried 
 	var carried = [];
-	for (var t = 0; t < carrierObjs.length; t++) {
-		for (var carrier = 0; carrier < carrierObjs[t].carrying.length; carrier++) {
-			if (!carried.includes(carrierObjs[t].carrying[carrier])) {
-				carried.push(carrierObjs[t].carrying[carrier]);
+	carrierObjs.forEach(carrier => {
+		carrier.carrying.forEach(tow => {
+			if (!carried.includes(tow)) {
+				carried.push(tow);
 			}
-		}
-	}
+		});
+	});
+
 	
 	//Find the tower that no one is carrying
 	for (var c = 0; c < carrierObjs.length; c++) {
@@ -42,7 +36,7 @@ function bottomTower(towers) {
 }
 
 //Object constructor for towers
-function tower(name, weight, carrying) {
+function Tower(name, weight, carrying) {
 	this.name = name;
 	this.weight = +weight.match(/\d+/)[0];
 	this.carrying = carrying;
