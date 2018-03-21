45d800aa488f0f66dcdb48fbbdca29b1d495de8c
Marla Glasser <32146632+mglasser@users.noreply.github.com>
2018-03-21 18:35:56 -0400

    added function for visualizing groups

--- src/Day14-Puzzle2.js
+++ src/Day14-Puzzle2.js
@@ -1,5 +1,3 @@
-//Answer is between 1061 and 1562
-
 //Advent of Code - Day 14 - Puzzle 2
 //Convert input-0 ... input-127 via asciiHash(Day10)
 //Convert each hex output binary (4-digits to 1 hex)
@@ -29,83 +27,6 @@ function createBinaryGrid(input) {
 	
 }
 
-//Takes a 2D array of 1s and 0s 
-//Returns number of contiguous groups of 1s
-function groupCount(input) {
-	let binaryGrid = createBinaryGrid(input);
-	let width = binaryGrid[0].length;
-	let height = binaryGrid.length;
-	/*binaryGrid = [
-		[0,1,1,0,0,1,1,0],
-		[1,0,1,1,0,1,0,0],
-		[1,1,0,1,0,1,0,1],
-		[0,1,0,1,1,1,0,1],
-		[1,0,0,0,0,0,0,0],
-		[0,1,0,0,0,1,1,1],
-		[1,1,0,1,1,0,0,1],
-		[1,0,1,0,1,0,1,1]];*/
-	
-	var groups = 0;
-	for (var row = 0; row < height; row++) {
-		for (var col = 0; col < width; col++) {
-			if (binaryGrid[row][col] > 0) {
-				if (binaryGrid[row][col] == 1) {
-					//Check if cell to right is already in a counted group
-					if (col != width - 1 && binaryGrid[row][col + 1] > 1) { 
-						//change cell to adjacent group#
-						binaryGrid[row][col] = binaryGrid[row][col + 1];
-					}
-					else { //Start a new group
-						groups++;
-						binaryGrid[row][col] = groups + 1;
-					}
-				}
-				//Check if you're in the middle of a duplicate group
-				else if (col != width - 1 && binaryGrid[row][col + 1] > 1 
-							&& binaryGrid[row][col] != binaryGrid[row][col + 1]) {
-					//console.log(`duplicate at [${row}][${col}]`);
-					binaryGrid[row][col + 1] = binaryGrid[row][col];
-					groups--;
-				}
-
-				//Change adjacent 1s to group#
-				if (col != width - 1 && binaryGrid[row][col + 1] == 1) {
-					binaryGrid[row][col + 1] = binaryGrid[row][col];
-				}
-				if (row != width - 1 && binaryGrid[row + 1][col] == 1) {
-					binaryGrid[row + 1][col] = binaryGrid[row][col];
-				}
-			}
-			
-		}
-	}
-
-	console.log(groups);
-	return binaryGrid;
-	/*
-	console.log(binaryGrid);
-	return groups;
-	*/
-}
-
-//RETURNS THE HTML FOR A VISUAL REPRESENTATION OF THE GROUPED GRID
-function visualGrid(input) {
-	let visualGrid = groupCount(input);
-	let HTML = "";
-	visualGrid.forEach(row => {
-		let rowHTML = '<div class="row">';
-			row.forEach(elem => {
-				let hex = (elem*10000).toString(16).padStart(6,"0");
-				let color = elem == 0 ? '#FFFFFF' : '#' + hex;
-				rowHTML += `<div class="box" style="background-color: ${color};"></div>`;
-			});
-		rowHTML += '</div>';
-		HTML += rowHTML;
-	})
-	return HTML;
-}
-
-//WHOLE NEW APPROACH
 function pipeMethod(input) {
 	let binaryGrid = createBinaryGrid(input);
 	let width = binaryGrid[0].length;
@@ -166,4 +87,113 @@ function defragGroups(obj) {
 	}
 	return groups;
 }
+
+//RETURNS THE HTML FOR A VISUAL REPRESENTATION OF THE GROUPED GRID
+function makeVisualGrid(input) {
+	/* pipeMethod except return statement */
+	let binaryGrid = createBinaryGrid(input);
+	let width = binaryGrid[0].length;
+	let height = binaryGrid.length;
+
+	let visualGrid = Array(height).fill(Array(width).fill('#FFFFFF'));
+
+	//Make ones an object in the form <location of 1>: [array of loc of adjacent 1s]
+	//grid is numbered L-R then T-B from 0 to width*height
+	//example: 128 per row, the second item in the second row is a 1, so is the item above it and to the right:
+	//129: [1,130]
+	let ones = {}, solos = [];
+	binaryGrid.forEach((row,whichRow,grid) => {
+		row.forEach((elem,index) => {
+			if (elem == 1) {
+				let spot = whichRow * width + index;
+				let touches = [];
+				//to the left
+				if (index > 0 && row[index - 1] == 1) {
+					touches.push(whichRow * width + index - 1);
+				}
+				//to the right
+				if (index < width - 1 && row[index + 1] == 1) {
+					touches.push(whichRow * width + index + 1);
+				}
+				//above
+				if (whichRow > 0 && grid[whichRow - 1][index] == 1) {
+					touches.push((whichRow - 1) * width + index);
+				}
+				//below
+				if (whichRow < height - 1 && grid[whichRow + 1][index] == 1) {
+					touches.push((whichRow + 1) * width + index);
+				}
+
+				if (touches.length > 0) {
+					ones[spot] = touches;
+				}
+				else solos.push(spot);
+			}
+		})
+	})
+	/* end pipeMethod */
+
+	//Make each solo a different color in visual grid
+	let color = 16 ** 6 - 1000000;
+	visualGrid = visualGrid.map((row,whichRow) => {
+		return row.map((cell, index) => {
+			if (solos.includes(whichRow * width + index)) {
+				color -= 40000;
+				return color.toString(16).padStart(6,'0');
+			}
+			else return cell;
+		})
+	})
+
+
+	/* defragGroups without counting groups */
+	var props = Object.keys(ones).map(elem => +elem);
+
+	//Run talkNumGroup on first elem in props
+	//Remove all elements in props[0]'s group; groups++
+	//Keep running until no elements remain in prop
+	color = 10000;
+	while (props.length > 0) {
+		var found =	talkNumGroup(ones,props[0]);
+		//change found's indices in visualGrid to color's hex value
+		visualGrid = visualGrid.map((row,whichRow) => {
+			return row.map((cell, index) => {
+				if (found.includes(whichRow * width + index)) {
+					return color.toString(16).padStart(6,'0');
+				}
+				else return cell;
+			});
+		});
+		color += 20000;
+		props = props.filter(elem => !(found).includes(elem));
+	}
+	/* end defragGroups */
+
+	console.log(visualGrid);
+
+	let HTML = "";
+	visualGrid.forEach(row => {
+		let rowHTML = '<div class="row">';
+			row.forEach(elem => {
+				rowHTML += `<div class="box" style="background-color: #${elem};"></div>`;
+			});
+		rowHTML += '</div>';
+		HTML += rowHTML;
+	})
+	return HTML;
+}
+
+//Returns [row,col] corresponding to num's place in a width x height matrix
+function matrixValue(num,width,height) {
+	let row = Math.floor(num / height);
+	let col = num % width;
+	return [row,col];
+}
+
+//Takes the row and col values in a square matrix with width <width>
+//Returns an int representing where it is (L-R, T-B)
+function reverseMatrixVal(row,col,width) {
+	return row*width + col;
+}
+
 
